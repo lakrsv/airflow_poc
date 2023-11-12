d7d3514e043a309eee3c5935674c9acb7d0c64fc
Lars-Kristian Svenøy <lakrsv@gmail.com>
2023-11-12 17:53:56 +0000

    with syntax

--- dags/spark_pi.py
+++ dags/spark_pi.py
@@ -38,22 +38,19 @@ default_args = {
 
 # [START instantiate_dag]
 
-dag = DAG(
+with DAG(
     'spark_pi',
     start_date=days_ago(1),
     default_args=default_args,
     schedule_interval=timedelta(days=1),
     tags=['example']
-)
-
-submit = SparkKubernetesOperator(
-    task_id='spark_transform_data',
-    namespace='spark-operator',
-    application_file='spark-pi.yaml',
-    kubernetes_conn_id='kubernetes_default',
-    do_xcom_push=True,
-)
-
-
+):
+    submit = SparkKubernetesOperator(
+        task_id='spark_transform_data',
+        namespace='spark-operator',
+        application_file='spark-pi.yaml',
+        kubernetes_conn_id='kubernetes_default',
+        do_xcom_push=True,
+    )
 
 submit
