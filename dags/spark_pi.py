724ff2fd3f9ddab4059b5a3311590c4d30f73ead
Lars-Kristian Svenøy <lakrsv@gmail.com>
2023-11-12 18:37:49 +0000

    Change job namespace

--- dags/spark_pi.py
+++ dags/spark_pi.py
@@ -47,7 +47,7 @@ with DAG(
 ):
     submit = SparkKubernetesOperator(
         task_id='spark_transform_data',
-        namespace='spark-operator',
+        namespace='spark',
         application_file='spark-pi.yaml',
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
