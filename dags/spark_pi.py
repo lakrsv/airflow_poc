59f3ac120929d815e80cbc09f8ed1193b9e2e0fe
Lars-Kristian Svenøy <lakrsv@gmail.com>
2023-11-12 17:31:22 +0000

    Move yaml file

--- dags/spark_pi.py
+++ dags/spark_pi.py
@@ -49,7 +49,7 @@ dag = DAG(
 submit = SparkKubernetesOperator(
     task_id='spark_transform_data',
     namespace='spark-operator',
-    application_file='../spark-operator/spark-pi.yaml',
+    application_file='spark-pi.yaml',
     kubernetes_conn_id='kubernetes_default',
     do_xcom_push=True,
 )
